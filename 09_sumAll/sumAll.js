4ddb8a08827f13d6480efec9aa0a663a2c3e23d8
Kai Hong <ricedev10@gmail.com>
2025-10-28 07:39:55 -0400

    finish 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (startInteger, endInteger) {
+	if (startInteger < 0 || endInteger < 0) return "ERROR";
+	if (
+		typeof startInteger != "number" ||
+		typeof endInteger != "number"
+	)
+		return "ERROR";
+	if (
+		Math.floor(startInteger) != startInteger ||
+		Math.floor(endInteger) != endInteger
+	)
+		return "ERROR";
 
+	if (endInteger < startInteger) {
+		[startInteger, endInteger] = [endInteger, startInteger];
+	}
+
+	let total = 0;
+	for (let i = startInteger; i < endInteger + 1; i++) {
+		total += i;
+	}
+
+	return total;
 };
 
 // Do not edit below this line
