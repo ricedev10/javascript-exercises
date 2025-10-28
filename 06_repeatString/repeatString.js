f78079872a99bc06edea29421568f6e594d22824
Kai Hong <ricedev10@gmail.com>
2025-10-27 21:32:28 -0400

    complete 6 & 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (string, count) {
+	let repeat = "";
+	for (let i = 0; i < count; i++) repeat += string;
+	return count < 0 ? "ERROR" : repeat;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+	let characters = string.split("");
+	characters.reverse();
 
+	return characters.join("");
 };
-
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
